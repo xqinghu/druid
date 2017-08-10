2871f57d2e65c11c7bfb953deca6b92216f11dc9
Gian Merlino <gianmerlino@gmail.com>
2017-08-09 17:20:00 -0700

    Replace Guava Enum.getIfPresent with builtin version. (#4659)
    * Replace Guava Enum.getIfPresent with builtin version.
    
    This is useful for running in Hadoop environments that use Guava 11. Some
    code is also simplified.
    
    * Code review

--- processing/src/main/java/io/druid/query/groupby/GroupByQueryHelper.java
+++ processing/src/main/java/io/druid/query/groupby/GroupByQueryHelper.java
@@ -19,11 +19,11 @@
 
 package io.druid.query.groupby;
 
-import com.google.common.base.Enums;
 import com.google.common.base.Function;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import io.druid.collections.NonBlockingPool;
+import io.druid.common.guava.GuavaUtils;
 import io.druid.data.input.MapBasedInputRow;
 import io.druid.data.input.MapBasedRow;
 import io.druid.data.input.Row;
@@ -242,9 +242,12 @@ public class GroupByQueryHelper
 
     for (AggregatorFactory aggregatorFactory : query.getAggregatorSpecs()) {
       final String typeName = aggregatorFactory.getTypeName();
-      final ValueType valueType = typeName != null
-                                  ? Enums.getIfPresent(ValueType.class, typeName.toUpperCase()).orNull()
-                                  : null;
+      final ValueType valueType;
+      if (typeName != null) {
+        valueType = GuavaUtils.getEnumIfPresent(ValueType.class, typeName.toUpperCase());
+      } else {
+        valueType = null;
+      }
       if (valueType != null) {
         types.put(aggregatorFactory.getName(), valueType);
       }
