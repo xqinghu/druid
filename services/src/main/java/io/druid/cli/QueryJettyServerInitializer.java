9a620cd46d89d9a746f579368cb484e63b008420
Jonathan Wei <jon-wei@users.noreply.github.com>
2017-11-15 12:41:30 -0800

    Add unsecured /health endpoint, remove auth checks from isLeader (#5087)
    * Add unsecured /health endpoint, remove auth checks from isLeader
    
    * PR comments

--- services/src/main/java/io/druid/cli/QueryJettyServerInitializer.java
+++ services/src/main/java/io/druid/cli/QueryJettyServerInitializer.java
@@ -21,6 +21,7 @@ package io.druid.cli;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Key;
@@ -47,7 +48,10 @@ import java.util.Set;
  */
 public class QueryJettyServerInitializer implements JettyServerInitializer
 {
-  private static Logger log = new Logger(QueryJettyServerInitializer.class);
+  private static final Logger log = new Logger(QueryJettyServerInitializer.class);
+  private static final List<String> UNSECURED_PATHS = Lists.newArrayList(
+      "/status/health"
+  );
 
   private final List<Handler> extensionHandlers;
 
@@ -69,6 +73,10 @@ public class QueryJettyServerInitializer implements JettyServerInitializer
 
     List<Authenticator> authenticators = null;
     AuthenticationUtils.addSecuritySanityCheckFilter(root, jsonMapper);
+
+    // perform no-op authorization for these resources
+    AuthenticationUtils.addNoopAuthorizationFilters(root, UNSECURED_PATHS);
+
     authenticators = authenticatorMapper.getAuthenticatorChain();
     AuthenticationUtils.addAuthenticationFilterChain(root, authenticators);
 
