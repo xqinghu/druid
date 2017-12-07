2427110fd3b766710f948f67881ad9879521d96f
Gian Merlino <gianmerlino@gmail.com>
2017-12-06 18:37:02 -0800

    Fix some unemitted alerts. (#5141)

--- server/src/main/java/io/druid/metadata/SQLMetadataSegmentManager.java
+++ server/src/main/java/io/druid/metadata/SQLMetadataSegmentManager.java
@@ -476,7 +476,7 @@ public class SQLMetadataSegmentManager implements MetadataSegmentManager
                             ));
                           }
                           catch (IOException e) {
-                            log.makeAlert(e, "Failed to read segment from db.");
+                            log.makeAlert(e, "Failed to read segment from db.").emit();
                             return null;
                           }
                         }
